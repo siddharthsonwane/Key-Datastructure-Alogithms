e3efc295b40b0e7415c9b871781845b4c9e3a797
siddharthsonwane <siddharthsonwane18@gmail.com>
2025-07-10 23:20:25 +0530

    Linkedlist code added

---
+++ src/main/linkedlist/LinkedList.java
@@ -0,0 +1,21 @@
+package main.linkedlist;
+
+import main.common.Node;
+
+public class LinkedList {
+    private Node head;
+    private Node tail;
+    private int length;
+
+    public LinkedList(int value){
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+
+    public void printList(){
+        
+    }
+
+}
