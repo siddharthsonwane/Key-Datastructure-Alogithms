117212493b7ba85bb34d255667eb0b574c7609d0
siddharthsonwane <siddharthsonwane18@gmail.com>
2025-07-10 23:27:17 +0530

    Stack and Queue refactoring

---
+++ src/main/stack/StackRunner.java
@@ -0,0 +1,21 @@
+package main.stack;
+
+public class StackRunner {
+    public static void main(String[] args) {
+        Stack myStack = new Stack(2);   // Creates a stack with initial value 2
+
+        myStack.printStack();
+        myStack.getTop();
+        myStack.getHeight();
+
+        myStack.push(4);
+        myStack.push(6);
+
+        myStack.printStack();
+        myStack.getTop();
+        myStack.getHeight();
+
+        myStack.pop();
+        myStack.printStack();
+    }
+}
